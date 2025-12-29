2808db6f8b1d703159d0592a0738b664892ed1de
Adarsh Singh Tomar <tomaradarsh18@gmail.com>
2025-12-29 09:31:00 +0530

    Real Time Engine implimented

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/AdarshGT33/ednr/adapters"
 	"github.com/AdarshGT33/ednr/events"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -14,6 +16,7 @@ import (
 
 var ctx = context.Background()
 var rdb *redis.Client
+var adapter map[string]adapters.NotificationAdapter
 
 func main() {
 	rdb = redis.NewClient(&redis.Options{
@@ -25,6 +28,11 @@ func main() {
 		log.Fatalf("Error loading .env file: %s\n", err)
 	}
 
+	adapter = map[string]adapters.NotificationAdapter{
+		"email": adapters.NewEmailAdapter(),
+		"sms":   adapters.NewSMSAdapter(),
+	}
+
 	go event_processor()
 
 	r := gin.Default()
@@ -33,8 +41,19 @@ func main() {
 		var event events.Events
 		if err := c.BindJSON(&event); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
-		c.JSON(http.StatusOK, gin.H{"status": "received"})
+
+		if _, exists := adapter[event.Channel]; !exists {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": fmt.Sprintf("unknown channel: %s", event.Channel),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "queued",
+			"channel": event.Channel,
+		})
 	})
 
 	r.Run(":8080")
@@ -49,13 +68,28 @@ func event_processor() {
 		//Right means pop from the right end of the queue
 		result, err := rdb.BRPop(ctx, 0, "event_queue").Result()
 		if err != nil {
+			fmt.Printf("Error popping from queue: %v\n", err)
 			continue
 		}
 
 		event_json := result[1]
 		var event events.Events
-		json.Unmarshal([]byte(event_json), &event)
+		if err := json.Unmarshal([]byte(event_json), &event); err != nil {
+			fmt.Printf("Error parsing event: %v\n", err)
+			continue
+		}
 
 		//sending events on the basis of severity
+		adap_er, exists := adapter[event.Channel]
+		if !exists {
+			fmt.Printf("Unknown channel: %s\n", event.Channel)
+		}
+
+		if err := adap_er.Send(event.Recipient, event.Message); err != nil {
+			fmt.Printf("Failed to send: %v\n", err)
+			//impliment DLQ here
+		} else {
+			fmt.Printf("Event successfully send: %s\n", event.Channel)
+		}
 	}
 }
